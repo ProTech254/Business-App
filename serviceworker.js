6f9e9e524404c8efff1c318ee09593966e7aba37
james waweru <44203130+jaywezz@users.noreply.github.com>
2021-11-15 15:58:05 +0300

    manifest

--- serviceworker.js
+++ serviceworker.js
@@ -1,51 +1,46 @@
-var cacheAll = false;
-var CACHE_NAME = 'webapk-cache';
-// var urlsToCache = [
-// 	'/',
-// 	'/wp-content/uploads/2020/08/cropped-SC_Logo-192x192.png',
-// 	'/wp-content/uploads/2020/08/Supercoders-2x1_Transparent1.png',
-// ];
-var urlsNotToCache = [
+const cacheName = 'business_app-v1';
+const staticAssets = [
+    './',
+    './index.html',
+    './manifest.webmanifest'
 ];
 
-// Install Event
-self.addEventListener('install', function(event) {
-	console.log("[SW] install event: ",event);
-	// Perform install steps
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(
-			function(cache) {
-				console.log('[SW] Opened cache: ',cache);
-				return cache.addAll(urlsToCache);
-			}
-		)
-	);
+self.addEventListener('install', async e => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
 });
 
-
-// Fetch Event
-self.addEventListener('fetch', function(event) {
-	console.log("[SW] fetch event: ",event);
-	event.respondWith(
-		caches.match(event.request).then(
-			function(response) {
-				if (response) return response;
-				else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
-				else {
-					fetch(event.request).then(
-						function(response) {
-							if(!response || response.status !== 200 || response.type !== 'basic') return response;
-							var responseToCache = response.clone();
-							caches.open(CACHE_NAME).then(
-								function(cache) {
-									cache.put(event.request, responseToCache);
-								}
-							);
-							return response;
-						}
-					);
-				}
-			}
-		)
-	);
+self.addEventListener('activate', e => {
+    self.clients.claim();
 });
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    }else {
+        e.respondWith(networkAndCache(req));
+    }
+
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+} 
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
